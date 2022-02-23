5002fab5eb16878954b9eb5bfddf91559e63dde8
Fabio Santos <fabio.profissional@yahoo.com.br>
2022-02-22 21:20:29 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,61 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import List from './components/List';
-import withListLoading from './components/WithListLoading';
-import http from "./services/http";
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import UserDetails from "./components/user-details";
+import UsersList from "./components/users-list";
 
-function App() {
-  const ListLoading = withListLoading(List);
-  const [appState, setAppState] = useState({
-    loading: false,
-    users: null,
-  });
-
-  function previous() {
-    let previousPage = appState.users[0].id - 30
-    
-    setAppState({ loading: true });
-      http
-      .get(`/users?since=${previousPage}`)
-      .then((response) => {
-        setAppState({ loading: false, users: response.data });
-      });
-  }
-
-  function next() {
-    let lastElement = appState.users[appState.users.length - 1]
-    
-    setAppState({ loading: true });
-      http
-      .get(`/users?since=${lastElement.id}`)
-      .then((response) => {
-        setAppState({ loading: false, users: response.data });
-      });
-  }
-
-  useEffect(() => {
-    setAppState({ loading: true });
-      http
-      .get("/users")
-      .then((response) => {
-        setAppState({ loading: false, users: response.data });
-      });
-  }, [setAppState]);
-  return (
-    <div className='App'>
-      <div className='container'>
-        <h1>Users</h1>
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path={["/", "/userslist"]} component={UsersList} />
+            <Route path="/userdetails/:id" component={UserDetails} />
+          </Switch>
+        </div>
       </div>
-      <div className='users-container'>
-        <ListLoading isLoading={appState.loading} users={appState.users} />
-        <button onClick={previous}>
-          Previous
-        </button>
-        <button onClick={next}>
-          Next
-        </button>
-      </div>
-    </div>
-  );
+    );
+  }
 }
+
 export default App;
